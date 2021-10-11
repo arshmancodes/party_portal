5cf39636ca090cbb8987e9000f5e87727fb7a44f
arshmancodes <arshman3@gmail.com>
2021-10-12 02:32:00 +0500

    update

---
+++ lib/models/partymodel.dart
@@ -0,0 +1,86 @@
+class PartyModel {
+  String? createdBy;
+  String? partySize;
+  int? hostCount;
+  int? guestCount;
+  List<String>? partyImages;
+  String? partyInfo;
+  String? startingDate;
+  String? endingDate;
+  Location? location;
+  List<String>? drinks;
+  List<String>? games;
+  String? partyRating;
+
+  PartyModel(
+      {this.createdBy,
+      this.partySize,
+      this.hostCount,
+      this.guestCount,
+      this.partyImages,
+      this.partyInfo,
+      this.startingDate,
+      this.endingDate,
+      this.location,
+      this.drinks,
+      this.games,
+      this.partyRating});
+
+  PartyModel.fromJson(Map<String, dynamic> json) {
+    createdBy = json['createdBy'];
+    partySize = json['partySize'];
+    hostCount = json['hostCount'];
+    guestCount = json['guestCount'];
+    partyImages = json['partyImages'].cast<String>();
+    partyInfo = json['partyInfo'];
+    startingDate = json['startingDate'];
+    endingDate = json['endingDate'];
+    location = json['location'] != null
+        ? new Location.fromJson(json['location'])
+        : null;
+    drinks = json['drinks'].cast<String>();
+    games = json['games'].cast<String>();
+    partyRating = json['partyRating'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['createdBy'] = this.createdBy;
+    data['partySize'] = this.partySize;
+    data['hostCount'] = this.hostCount;
+    data['guestCount'] = this.guestCount;
+    data['partyImages'] = this.partyImages;
+    data['partyInfo'] = this.partyInfo;
+    data['startingDate'] = this.startingDate;
+    data['endingDate'] = this.endingDate;
+    if (this.location != null) {
+      data['location'] = this.location!.toJson();
+    }
+    data['drinks'] = this.drinks;
+    data['games'] = this.games;
+    data['partyRating'] = this.partyRating;
+    return data;
+  }
+}
+
+class Location {
+  double? latitude;
+  double? longitude;
+  String? locationName;
+
+  Location({this.latitude, this.longitude, this.locationName});
+
+  Location.fromJson(Map<String, dynamic> json) {
+    latitude = json['latitude'];
+    longitude = json['longitude'];
+    locationName = json['locationName'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['latitude'] = this.latitude;
+    data['longitude'] = this.longitude;
+    data['locationName'] = this.locationName;
+    return data;
+  }
+}
